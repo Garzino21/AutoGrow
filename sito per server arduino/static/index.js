be96096af4a9d39eeb711c79e48d95f529b77589
Garzino21 <92369567+Garzino21@users.noreply.github.com>
2024-02-22 21:57:05 +0100

    aggiunti importanti cambiamenti grafici e trasmissione

---
+++ sito per server arduino/static/index.js
@@ -0,0 +1,61 @@
+"use strict"
+$(document).ready(function () {
+
+    //variabili
+    let _apri = $("#apri");
+    let _paginaIniziale = $("#paginaIniziale");
+    let _paginaDati = $("#paginaDati");
+    let _progetto=$("#progetto");
+    let _info = $("#info");
+    let _body = $("body");
+    let _indietro = $("#indietro"); 
+
+
+    //impostazioni di avvio
+    _paginaIniziale.show().css("margin-top", "flex");
+    _paginaDati.hide();
+    _progetto.hide();
+    _indietro.hide();
+
+    //gestione eventi
+    _apri.on("click", function () {
+        _paginaIniziale.hide();
+        _paginaDati.show();
+        _info.hide();
+        _apri.hide();
+        _indietro.show();
+        _body.css("overflow", "scroll");
+    });
+
+    _info.on("click", function () {
+        _progetto.show();
+        _paginaIniziale.hide();
+        _indietro.show();
+        _body.css("overflow", "scroll");
+    })
+
+    _indietro.on("click", function () {
+        _paginaIniziale.show();
+        _paginaDati.hide();
+        _progetto.hide();
+        _info.show();
+        _apri.show();
+        _indietro.hide();
+        _body.css("overflow", "hidden");
+    })
+
+    //prendo dati dal db
+
+    let rq = inviaRichiesta("POST", "/api/dati")
+    rq.then(function (response) {
+        console.log(response.data)
+    })
+    rq.catch(function (err) {
+        if (err.response.status == 401) {
+            _lblErrore.show();
+        }
+        else
+            errore(err);
+    })
+
+});
---
+++ sketch_feb22a/sketch_feb22a.ino
@@ -0,0 +1,82 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-http-request
+ */
+
+#include <WiFiS3.h>
+
+const char ssid[] = "Wadsl-Garzino";  // change your network SSID (name)
+const char pass[] = "mafizama76";   // change your network password (use for WPA, or use as key for WEP)
+
+WiFiSSLClient client;
+int status = WL_IDLE_STATUS;
+
+int HTTP_PORT =443;
+String HTTP_METHOD = "GET";  // or POST
+char HOST_NAME[] = "garzino-crudserer.onrender.com";
+String PATH_NAME = "/api/getCollections";
+
+void setup() {
+  Serial.begin(9600);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true)
+      ;
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // print your board's IP address:
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // connect to web server on port 80:
+  if (client.connect(HOST_NAME, HTTP_PORT)) {
+    // if connected:
+    Serial.println("Connected to server");
+    // make a HTTP request:
+    // send HTTP header
+    client.println(HTTP_METHOD + " " + PATH_NAME + " HTTP/1.1");
+    client.println("Host: " + String(HOST_NAME));
+    client.println("Connection: close");
+    client.println();  // end HTTP header
+
+    while (client.connected()) {
+      if (client.available()) {
+        // read an incoming byte from the server and print it to serial monitor:
+        char c = client.read();
+        Serial.print(c);
+      }
+    }
+
+    // the server's disconnected, stop the client:
+    client.stop();
+    Serial.println();
+    Serial.println("disconnected");
+  } else {  // if not connected:
+    Serial.println("connection failed");
+  }
+}
+
+void loop() {
+}
